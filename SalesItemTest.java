efb16a7f2f15ee5c9578fa63cab21b640f330345
Marti Schuschel <schuschel@newsaktuell.de>
2019-11-12 11:02:02 +0100

    First Test Methods

---
+++ SalesItemTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalesItemTest {
+
+    @Test
+    void getName() {
+        SalesItem salesIte1 = new SalesItem("Java for Dummies", 5000);
+        assertEquals("Java for Dummies", salesIte1.getName());
+    }
+
+    @Test
+    void getPrice() {
+        SalesItem salesIte1 = new SalesItem("Java for Dummies", 5000);
+        assertEquals(5000, salesIte1.getPrice());
+}
+
+    @Test
+    void getNumberOfComments() {
+    }
+
+    @Test
+    void addComment() {
+        SalesItem salesIte1 = new SalesItem("Java for Dummies", 5000);
+        assertEquals(true, salesIte1.addComment("Herbert", "Super Duper", 5));
+
+    }
+
+    @Test
+    void findMostHelpfulComment() {
+    }
+}
